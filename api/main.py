3cbfa70dbe93e7db4a719fae992b2854fc5d8f7b
James Hoffman <jamesvhoffman@outlook.com>
2022-11-16 22:55:33 -0500

    Update routers

--- api/main.py
+++ api/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
-
+from authenticator import authenticator
+from routers import accounts
 
 app = FastAPI()
+app.include_router(authenticator.router)
+app.include_router(accounts.router)
